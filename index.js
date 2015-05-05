02d87980b2b07531e210c7f058e064f5cb80bf13
Guy <guy@codefresh.io>
2015-05-05 11:02:15 +0300

    newrelic monitor wrapper

---
+++ index.js
@@ -0,0 +1,41 @@
+var fs      = require('fs'),
+    path    = require('path');
+
+var newrelicFile = path.join(process.cwd(), 'newrelic.js');
+
+if (fs.existsSync(newrelicFile)) {
+    var nr = require('newrelic');
+
+    var config;
+
+    return module.exports = {
+
+        init: function(_config) {
+            config = _config;
+        },
+        recordMetric: function(ns, val) {
+            nr.recordMetric(ns, val);
+        },
+        noticeError: function(err) {
+            nr.noticeError(err);
+        },
+        endTransaction: function() {
+            nr.endTransaction();
+        },
+        createWebTransaction: function(url, handle) {
+            return nr.createWebTransaction(url, handle);
+        }
+    };
+}
+
+module.exports = {
+    init: function() {},
+    recordMetric: function() {},
+    noticeError: function() {},
+    endTransaction: function() {},
+    createWebTransaction: function(url, handle) {
+        return handle;
+    }
+};
+
+
